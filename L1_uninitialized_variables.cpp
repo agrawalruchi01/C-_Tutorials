0b5fb932d07ea9ff50d6e9f32bf9bac4df401ed0
Ruchi Agrawal <ruagrawa@microsoft.com>
2020-07-20 00:11:36 +0530

    Uninitialized variables : c++

---
+++ L1_uninitialized_variables.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+ // x will be intialize to garbage value, some compiler can give error that x is not initialized before use
+ int x;
+ cout<<x;
+}
